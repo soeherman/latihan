5700ada1379ea9b375435bea930ad652796d4dc0
soeherman <gatotkoco419@gmail.com>
2018-01-10 23:25:10 +0700

    Commit Pertama

---
+++ app/Http/Controllers/controlLatihan.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class controlLatihan extends Controller
+{
+    //
+    public class haha{
+
+    }
+
+    public class heha{
+    	
+    }
+}
